3c554df2e7ca84617e7eb9821c8881b3e735df85
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-01-03 18:56:44 +0000

    fixed leftovers

--- main.hs
+++ main.hs
@@ -11,9 +11,6 @@ nFullRows n = balance $ foldl insert Empty [0..(2 ^ n - 2)]
 
 main :: IO ()
 main = do
-    -- print tree
-    testInsert; testSearch; testEq;
-    where
-        tree = foldl insert Empty [6,0,2,5,4,7,3,1,8,9]
-        third = search tree 2
-        third' = Node 2 (newTree 7) (Node 8 (newTree 9) Empty)
+    testInsert;
+    testSearch;
+    testEq;
